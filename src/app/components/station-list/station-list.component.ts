154b4116ca9d5170804ec7973ed142e5396fb422
vaglefou <vagfou@protonmail.com>
2024-10-22 03:25:27 +0300

    components initialized

---
+++ src/app/components/station-list/station-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-station-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './station-list.component.html',
+  styleUrl: './station-list.component.scss'
+})
+export class StationListComponent {
+
+}
